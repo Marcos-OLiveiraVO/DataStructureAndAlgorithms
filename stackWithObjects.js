581e27079b9dee01358ee4131526a46ee306854a
Marcos Oliveira <marcosoliveira.rd@gmail.com>
2022-10-19 23:05:58 -0300

    stack with objects

--- stackWithObjects.js
+++ stackWithObjects.js
@@ -26,8 +26,37 @@ class Stack {
     delete this.items[this.count];
     return result;
   }
+
+  peek() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+
+    return this.items[this.count - 1];
+  }
+
+  clear() {
+    this.items = {};
+    this.count = 0;
+  }
+
+  toString() {
+    if (this.isEmpty()) {
+      return "";
+    }
+
+    let objString = `${this.item[0]}`;
+    for (let i = 1; i < this.count; i++) {
+      objString = `${objString}, ${this.items[i]}`;
+    }
+
+    return objString;
+  }
 }
 
 const stack = new Stack();
-stack.push(5);
-stack.push(8);
+// stack.push(5);
+// stack.push(8);
+console.log(Object.getOwnPropertyNames(stack));
+console.log(Object.keys(stack));
+console.log(stack.items);
