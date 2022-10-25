41dd4cded54dadc1da9a3c4b0216d95fe98ad580
Marcos Oliveira <marcosoliveira.rd@gmail.com>
2022-10-25 20:54:44 -0300

    feature: queue

---
+++ Fila/queue.js
@@ -0,0 +1,69 @@
+class Queue {
+  constructor() {
+    this.count = 0;
+    this.lowestCount = 0;
+    this.items = {};
+  }
+
+  enqueue(element) {
+    this.items[this.count] = element;
+    this.count++;
+  }
+
+  dequeue(element) {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+
+    const result = this.items[this.lowestCount];
+    delete this.items[this.lowestCount];
+    this.lowestCount++;
+    return result;
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    return this.items[this.lowestCount];
+  }
+
+  isEmpty() {
+    return this.count - this.lowestCount === 0;
+  }
+
+  size() {
+    return this.count - this.lowestCount;
+  }
+
+  clear() {
+    this.items = {};
+    this.count = 0;
+    this.lowestCount = 0;
+  }
+
+  toString() {
+    if (this.isEmpty()) {
+      return "";
+    }
+
+    let objString = `${this.items[this.lowestCount]}`;
+    for (let i = this.lowestCount + 1; i < this.count; i++) {
+      objString = `${objString}, ${this.items[i]}`;
+    }
+    return objString;
+  }
+}
+
+const queue = new Queue();
+// console.log(queue.isEmpty());
+
+queue.enqueue("john");
+queue.enqueue("Jack");
+queue.enqueue("Camila");
+console.log(queue.toString());
+console.log(queue.size());
+console.log(queue.isEmpty());
+queue.dequeue();
+queue.dequeue();
+console.log(queue.toString());
